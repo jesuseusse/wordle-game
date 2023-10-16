1567230c3df662619d55ee4fd4efdebcafbb62fa
Jesus <you@example.com>
2023-10-16 12:41:18 -0600

    Deply

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const Modal = ({ children }: Props) => {
   return (
-    <div className="absolute h-screen w-screen flex justify-center items-center modal">
+    <div className="absolute h-screen w-screen flex justify-center items-center modal z-[1000]">
       <div className="max-w-2xl border-solid border-2 border-gray rounded bg-white dark:bg-dark-backgroundBody p-4 dark:text-white">
         {children}
       </div>
